2a4569d96c42e236da5ac04946296268c46d8938
Ben Elan <benelan@protonmail.com>
2021-01-04 09:39:15 -0800

    initial upload

---
+++ webdev/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  rootDir: '../',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest/jest.setup.js', '<rootDir>/jest/fetch.mock.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/docs/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/jest/jest.css.js',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+}
