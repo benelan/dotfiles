75ab7f7a6a8766b71fbc78ed9bffd64c14b3f584
Ben Elan <belan@esri.com>
2022-11-05 00:51:49 -0700

    feat(lvim): bump and update config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,31 +1,18 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
-
 -- Additional Plugins
 lvim.plugins = {
-  --c{ "folke/tokyonight.nvim" },
-  -- { "sainnhe/gruvbox-material" }
-  { "morhetz/gruvbox" },
-  -- {
-  --   "folke/trouble.nvim",
-  --   cmd = "TroubleToggle",
-  -- },
+{ "sainnhe/gruvbox-material" }
+-- {"morhetz/gruvbox"} 
+-- {
+--   "folke/trouble.nvim",
+--   cmd = "TroubleToggle",
+-- },
 }
-
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "gruvbox"
-lvim.builtin.lualine.options.theme = "gruvbox"
+lvim.colorscheme = "gruvbox-material"
+lvim.builtin.lualine.options.theme = "gruvbox-material"
 -- lvim.builtin.theme.name = "gruvbox"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -45,18 +32,18 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-  -- for normal mode
-  n = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-  },
+    -- for input mode
+    i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev
+    },
+    -- for normal mode
+    n = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous
+    }
 }
 
 -- Use which-key to add extra bindings with the leader-key prefix
@@ -71,23 +58,19 @@ lvim.builtin.telescope.defaults.mappings = {
 --   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 -- }
 
-lvim.builtin.which_key.mappings["-"] = {
-  function()
+lvim.builtin.which_key.mappings["-"] = {function()
     local previous_buf = vim.api.nvim_get_current_buf()
     require("nvim-tree").open_replacing_current_buffer()
     require("nvim-tree").find_file(false, previous_buf)
-  end,
-  "NvimTree in place",
-}
+end, "NvimTree in place"}
 
-lvim.builtin.which_key.mappings["lh"] = { "<cmd>HideDiagnostic<CR>", "Hide Diagnostics" }
-lvim.builtin.which_key.mappings["lg"] = { "<cmd>ShowDiagnostic<CR>", "Show Diagnostics" }
+lvim.builtin.which_key.mappings["lh"] = {"<cmd>HideDiagnostic<CR>", "Hide Diagnostics"}
+lvim.builtin.which_key.mappings["lg"] = {"<cmd>ShowDiagnostic<CR>", "Show Diagnostics"}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 -- lvim.builtin.nvimtree.setup.actions.open_file.quit_on_open = true
@@ -95,51 +78,49 @@ lvim.builtin.nvimtree.setup.hijack_netrw = true
 lvim.builtin.nvimtree.setup.view.preserve_window_proportions = true
 
 lvim.builtin.nvimtree.setup = {
-  view = {
-    mappings = {
-      list = {
-        -- NOTE: default to editing the file in place, netrw-style
-        {
-          key = { "<C-e>", "o", "-" },
-          action = "edit_in_place",
-        },
-        -- NOTE: override the "split" to avoid treating nvim-tree
-        -- window as special. Splits will appear as if nvim-tree was a
-        -- regular window
-        {
-          key = "<C-v>",
-          action = "split_right",
-          action_cb = function(node)
-            vim.cmd("vsplit " .. vim.fn.fnameescape(node.absolute_path))
-          end,
-        },
-        {
-          key = "<C-x>",
-          action = "split_bottom",
-          action_cb = function(node)
-            vim.cmd("split " .. vim.fn.fnameescape(node.absolute_path))
-          end,
-        },
-        -- NOTE: override the "open in new tab" mapping to fix the error
-        -- that occurs there
-        {
-          key = "<C-t>",
-          action = "new_tab",
-          action_cb = function(node)
-            vim.cmd("tabnew " .. vim.fn.fnameescape(node.absolute_path))
-          end,
-        },
-      },
+    view = {
+        mappings = {
+            list = {
+              -- NOTE: default to editing the file in place, netrw-style
+            {
+                key = {"<C-e>", "o", "-"},
+                action = "edit_in_place"
+            },
+            -- NOTE: override the "split" to avoid treating nvim-tree
+            -- window as special. Splits will appear as if nvim-tree was a
+            -- regular window
+            {
+                key = "<C-v>",
+                action = "split_right",
+                action_cb = function(node)
+                    vim.cmd("vsplit " .. vim.fn.fnameescape(node.absolute_path))
+                end
+            }, {
+                key = "<C-x>",
+                action = "split_bottom",
+                action_cb = function(node)
+                    vim.cmd("split " .. vim.fn.fnameescape(node.absolute_path))
+                end
+            },
+            -- NOTE: override the "open in new tab" mapping to fix the error
+            -- that occurs there
+            {
+                key = "<C-t>",
+                action = "new_tab",
+                action_cb = function(node)
+                    vim.cmd("tabnew " .. vim.fn.fnameescape(node.absolute_path))
+                end
+            }}
+        }
     },
-  },
-  actions = {
-    change_dir = {
-      -- NOTE: netrw-style, do not change the cwd when navigating
-      enable = false,
-    },
-  },
+    actions = {
+        change_dir = {
+            -- NOTE: netrw-style, do not change the cwd when navigating
+            enable = false
+        }
+    }
 }
--- if you don't want all the parsers change this to a table of the ones you want
+
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
   "c",
@@ -150,197 +131,74 @@ lvim.builtin.treesitter.ensure_installed = {
   "python",
   "typescript",
   "tsx",
-  "css",
   "rust",
   "yaml",
 }
 
-lvim.builtin.treesitter.ignore_install = { "java", "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.ignore_install = {"java", "haskell"}
+lvim.builtin.treesitter.highlight.enable = true
 
--- generic LSP settings
-
--- -- make sure server will always be installed even if the server is in skipped_servers list
--- lvim.lsp.installer.setup.ensure_installed = {
---     "sumeko_lua",
---     "jsonls",
--- }
--- -- change UI setting of `LspInstallInfo`
--- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
--- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
--- lvim.lsp.installer.setup.ui.border = "rounded"
--- lvim.lsp.installer.setup.ui.keymaps = {
---     uninstall_server = "d",
---     toggle_server_expand = "o",
--- }
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- `:LvimInfo` lists which server(s) are skipped for the current filetype
 lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
-  return server ~= "eslint"
+    return server ~= "eslint"
 end, lvim.lsp.automatic_configuration.skipped_servers)
 
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- setup formatters, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  --   { command = "black", filetypes = { "python" } },
-  --   { command = "isort", filetypes = { "python" } },
-  {
-    --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    -- extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    -- filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "jsx", "vue", "svelte" },
-  },
-}
+formatters.setup {{
+    command = "prettier"
+}}
 
--- -- set additional linters
+-- setup additional linters
 local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "eslint_d",
-    -- filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "jsx", "vue", "svelte" }
-  }
-
-  --   { command = "flake8", filetypes = { "python" } },
-  --   {
-  --     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --     command = "shellcheck",
-  --     ---@usage arguments to pass to the formatter
-  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --     extra_args = { "--severity", "warning" },
-  --   },
-  --   {
-  --     command = "codespell",
-  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --     filetypes = { "javascript", "python" },
-  --   },
-}
-
-local code_actions = require "lvim.lsp.null-ls.code_actions"
-code_actions.setup {
-  {
+linters.setup {{
     command = "eslint_d"
-  },
-}
+}, {
+    command = "shellcheck",
+    extra_args = {"--severity", "warning"}
+}}
 
+-- setup code actions
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup {{
+    command = "eslint_d"
+}}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = { "*.json", "*.jsonc" },
-  -- enable wrap mode for json files only
-  command = "setlocal wrap",
+    pattern = {"*.json", "*.jsonc"},
+    -- enable wrap mode for json files only
+    command = "setlocal wrap"
 })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
-
 
 local function organize_imports()
-  local params = {
-    command = "_typescript.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-    title = ""
-  }
-  vim.lsp.buf.execute_command(params)
+    local params = {
+        command = "_typescript.organizeImports",
+        arguments = {vim.api.nvim_buf_get_name(0)},
+        title = ""
+    }
+    vim.lsp.buf.execute_command(params)
 end
 
-vim.lsp.buf.execute_command({ command = "_typescript.organizeImports", arguments = { vim.fn.expand("%:p") } })
+vim.lsp.buf.execute_command({
+    command = "_typescript.organizeImports",
+    arguments = {vim.fn.expand("%:p")}
+})
 
-vim.api.nvim_buf_create_user_command(vim.api.nvim_get_current_buf(), 'HideDiagnostic',
-  function()
+vim.api.nvim_buf_create_user_command(vim.api.nvim_get_current_buf(), 'HideDiagnostic', function()
     vim.diagnostic.hide()
     -- lvim.lsp.diagnostics.virtual_text = false
-  end,
-  { nargs = 0 }
-)
+end, {
+    nargs = 0
+})
 
-vim.api.nvim_buf_create_user_command(vim.api.nvim_get_current_buf(), 'ShowDiagnostic',
-  function()
+vim.api.nvim_buf_create_user_command(vim.api.nvim_get_current_buf(), 'ShowDiagnostic', function()
     vim.diagnostic.show()
     -- lvim.lsp.diagnostics.virtual_text = true
-  end,
-  { nargs = 0 }
-)
+end, {
+    nargs = 0
+})
 
-
-
-------------------------------------------------------------------------
------> Vim Keymappings                                                 |
-------------------------------------------------------------------------
-
-
-vim.cmd [[ 
-  " remove highlight from search term after pressing Return
-  nnoremap <silent> <CR> :noh<CR><CR>
-
-  " find/replace the current word
-  nnoremap <silent> <leader>* :%s/<c-r><c-w>/<c-r><c-w>/gc<c-f>$F/i
-
-  " Quickly open a buffer for javascript
-  map <leader>bjs :e ~/buffer.js<cr>
-
-  " Quickly open a buffer for markdown
-  map <leader>bmd :e ~/buffer.md<cr>
- ]]
-
-
-vim.cmd [[
-  function! VisualSelection(direction, extra_filter) range
-    let l:saved_reg = @"
-    execute "normal! vgvy"
-
-    let l:pattern = escape(@", "\\/.*'$^~[]")
-    let l:pattern = substitute(l:pattern, "\n$", "", "")
-
-    if a:direction == 'gv'
-        call CmdLine("Ack '" . l:pattern . "' " )
-    elseif a:direction == 'replace'
-        call CmdLine("%s" . '/'. l:pattern . '/')
-    endif
-
-    let @/ = l:pattern
-    let @" = l:saved_reg
-  endfunction
-
-  " Visual mode pressing * or # searches for the current selection
-  vnoremap <silent> * :<C-u>call VisualSelection('', '')<CR>/<C-R>=@/<CR><CR>
-  vnoremap <silent> # :<C-u>call VisualSelection('', '')<CR>?<C-R>=@/<CR><CR>
-]]
-
-vim.cmd [[
-  " toggle relative line numbers
-  nnoremap <silent> <leader>ln :set relativenumber!<CR>
-  
-  " Automatically switch back and forth
-  " between absolute and relative line numbers
-  augroup relative_line_numbers
-    autocmd!
-      autocmd FocusLost,InsertEnter * :set norelativenumber
-      autocmd FocusGained,InsertLeave * :set relativenumber
-  augroup END
-]]
+require "user.options"
+require "user.keymaps"
