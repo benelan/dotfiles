e6187385ad9b1d228f176a04e32a94604d93f617
Ben Elan <belan@esri.com>
2022-11-10 01:03:29 -0800

    chore(bash plugins): setup cd on exit for fff and misc cleanup

---
+++ .dotfiles/bash/plugins/fff.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# fff - https://github.com/dylanaraps/fff
+export FFF_FAV1=~/dev/calcite/calcite-components
+export FFF_FAV2=~/dev/dotfiles
+export FFF_FAV3=~/Documents/lists
+export FFF_FAV4=/usr/share
+export FFF_FAV5=/.dotfiles/bash/functions.bash
+export FFF_FAV6=~/.dotfiles/sh/aliases/general.alias.sh
+export FFF_FAV7=~/.bashrc
+export FFF_FAV8=~/.vimrc
+export FFF_FAV9=~/.config/nvim/init.lua
+
+f() {
+    fff "$@"
+    cd "$(cat "${XDG_CACHE_HOME:=${HOME}/.cache}/fff/.fff_d")"
+}
--- .dotfiles/bash/plugins/misc.bash
+++ .dotfiles/bash/plugins/misc.bash
@@ -1,22 +1,4 @@
 #!/usr/bin/env bash
 
-function ff() {
-  local fff=~/.dotfiles/bash/lib/fff.bash;
-  [ -r "$fff" ] && [ -f "$fff" ] && source "$fff";
-}
-
-function fasd() {
-  local fasd=~/.dotfiles/bash/lib/fasd.bash;
-  [ -r "$fasd" ] && [ -f "$fasd" ] && source "$fasd";
-}
-
-fasd_cache=~/.dotfiles/cache/fasd_startup
-if [ "$(type -P fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-  fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache";
-fi
-source "$fasd_cache";
-unset fasd_cache;
-
-
-
-[[ "$(type -P thefuck)" ]] && eval "$(thefuck --alias)";
+# thefuck - https://github.com/nvbn/thefuck
+[[ "$(type -P thefuck)" ]] && eval "$(thefuck --enable-experimental-instant-mode --alias plz)";
