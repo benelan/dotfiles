8dd8735111afc13e055a93517d3a3de8d52e5ee3
Ben Elan <belan@esri.com>
2022-11-13 15:50:56 -0800

    style(nvim): format and reorganize

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,14 +1,12 @@
 -- Additional Plugins
 lvim.plugins = {
-    { "sainnhe/gruvbox-material" }
+  { "sainnhe/gruvbox-material" }
 }
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "gruvbox-material"
-lvim.builtin.lualine.options.theme = "gruvbox-material"
--- lvim.builtin.theme.name = "gruvbox"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -27,18 +25,18 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
-    -- for input mode
-    i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev
-    },
-    -- for normal mode
-    n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous
-    }
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous
+  }
 }
 
 -- Use which-key to add extra bindings with the leader-key prefix
@@ -54,9 +52,9 @@ lvim.builtin.telescope.defaults.mappings = {
 -- }
 
 lvim.builtin.which_key.mappings["-"] = { function()
-    local previous_buf = vim.api.nvim_get_current_buf()
-    require("nvim-tree").open_replacing_current_buffer()
-    require("nvim-tree").find_file(false, previous_buf)
+  local previous_buf = vim.api.nvim_get_current_buf()
+  require("nvim-tree").open_replacing_current_buffer()
+  require("nvim-tree").find_file(false, previous_buf)
 end, "NvimTree in place" }
 
 lvim.builtin.which_key.mappings["lh"] = { "<cmd>HideDiagnostic<CR>", "Hide Diagnostics" }
@@ -73,62 +71,62 @@ lvim.builtin.nvimtree.setup.hijack_netrw = true
 lvim.builtin.nvimtree.setup.view.preserve_window_proportions = true
 
 lvim.builtin.nvimtree.setup = {
-    view = {
-        mappings = {
-            list = {
-                -- NOTE: default to editing the file in place, netrw-style
-                {
-                    key = { "<C-e>", "o", "-" },
-                    action = "edit_in_place"
-                },
-                -- NOTE: override the "split" to avoid treating nvim-tree
-                -- window as special. Splits will appear as if nvim-tree was a
-                -- regular window
-                {
-                    key = "<C-v>",
-                    action = "split_right",
-                    action_cb = function(node)
-                        vim.cmd("vsplit " .. vim.fn.fnameescape(node.absolute_path))
-                    end
-                }, {
-                    key = "<C-x>",
-                    action = "split_bottom",
-                    action_cb = function(node)
-                        vim.cmd("split " .. vim.fn.fnameescape(node.absolute_path))
-                    end
-                },
-                -- NOTE: override the "open in new tab" mapping to fix the error
-                -- that occurs there
-                {
-                    key = "<C-t>",
-                    action = "new_tab",
-                    action_cb = function(node)
-                        vim.cmd("tabnew " .. vim.fn.fnameescape(node.absolute_path))
-                    end
-                }
-            }
+  view = {
+    mappings = {
+      list = {
+        -- NOTE: default to editing the file in place, netrw-style
+        {
+          key = { "<C-e>", "o", "-" },
+          action = "edit_in_place"
+        },
+        -- NOTE: override the "split" to avoid treating nvim-tree
+        -- window as special. Splits will appear as if nvim-tree was a
+        -- regular window
+        {
+          key = "<C-v>",
+          action = "split_right",
+          action_cb = function(node)
+            vim.cmd("vsplit " .. vim.fn.fnameescape(node.absolute_path))
+          end
+        }, {
+          key = "<C-x>",
+          action = "split_bottom",
+          action_cb = function(node)
+            vim.cmd("split " .. vim.fn.fnameescape(node.absolute_path))
+          end
+        },
+        -- NOTE: override the "open in new tab" mapping to fix the error
+        -- that occurs there
+        {
+          key = "<C-t>",
+          action = "new_tab",
+          action_cb = function(node)
+            vim.cmd("tabnew " .. vim.fn.fnameescape(node.absolute_path))
+          end
         }
-    },
-    actions = {
-        change_dir = {
-            -- NOTE: netrw-style, do not change the cwd when navigating
-            enable = false
-        }
+      }
     }
+  },
+  actions = {
+    change_dir = {
+      -- NOTE: netrw-style, do not change the cwd when navigating
+      enable = false
+    }
+  }
 }
 
 lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "css",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "rust",
-    "yaml",
+  "bash",
+  "c",
+  "css",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "rust",
+  "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "java", "haskell" }
@@ -137,57 +135,57 @@ lvim.builtin.treesitter.highlight.enable = true
 -- setup formatters, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { {
-    command = "prettier"
+  command = "prettier"
 } }
 
 -- setup additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { {
-    command = "eslint_d"
+  command = "eslint_d"
 }, {
-    command = "shellcheck",
-    extra_args = { "--severity", "warning" }
+  command = "shellcheck",
+  extra_args = { "--severity", "warning" }
 } }
 
 -- setup code actions
 local code_actions = require "lvim.lsp.null-ls.code_actions"
 code_actions.setup { {
-    command = "eslint_d"
+  command = "eslint_d"
 } }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = { "*.json", "*.jsonc" },
-    -- enable wrap mode for json files only
-    command = "setlocal wrap"
+  pattern = { "*.json", "*.jsonc" },
+  -- enable wrap mode for json files only
+  command = "setlocal wrap"
 })
 
 local function organize_imports()
-    local params = {
-        command = "_typescript.organizeImports",
-        arguments = { vim.api.nvim_buf_get_name(0) },
-        title = ""
-    }
-    vim.lsp.buf.execute_command(params)
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = ""
+  }
+  vim.lsp.buf.execute_command(params)
 end
 
 vim.lsp.buf.execute_command({
-    command = "_typescript.organizeImports",
-    arguments = { vim.fn.expand("%:p") }
+  command = "_typescript.organizeImports",
+  arguments = { vim.fn.expand("%:p") }
 })
 
 vim.api.nvim_buf_create_user_command(vim.api.nvim_get_current_buf(), 'HideDiagnostic', function()
-    vim.diagnostic.hide()
-    -- lvim.lsp.diagnostics.virtual_text = false
+  vim.diagnostic.hide()
+  -- lvim.lsp.diagnostics.virtual_text = false
 end, {
-    nargs = 0
+  nargs = 0
 })
 
 vim.api.nvim_buf_create_user_command(vim.api.nvim_get_current_buf(), 'ShowDiagnostic', function()
-    vim.diagnostic.show()
-    -- lvim.lsp.diagnostics.virtual_text = true
+  vim.diagnostic.show()
+  -- lvim.lsp.diagnostics.virtual_text = true
 end, {
-    nargs = 0
+  nargs = 0
 })
 
 require "user.options"
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -43,18 +43,10 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- General
+  -- Load first
   use { "wbthomason/packer.nvim", commit = "6afb67460283f0e990d35d229fd38fdc04063e0a" } -- the packer manage itself
   use { "nvim-lua/plenary.nvim", commit = "4b7e52044bbb84242158d977a50c4cbcd85070c7" } -- useful lua functions used by lots of plugins
   use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
-  use { "windwp/nvim-autopairs", commit = "4fc96c8f3df89b6d23e5092d31c866c53a346347" } -- autopairs, integrates with both cmp and treesitter
-  use { "numToStr/Comment.nvim", commit = "97a188a98b5a3a6f9b1b850799ac078faa17ab67" }
-  use { "ahmedkhalf/project.nvim", commit = "628de7e433dd503e782831fe150bb750e56e55d6" }
-  use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
-  use {"kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async", commit = "5da70eb121a890df8a5b25e6cc30d88665af97b8" }
-
-  -- Colorschemes
-  use { "sainnhe/gruvbox-material", commit = "2807579bd0a9981575dbb518aa65d3206f04ea02" }
 
   -- UI
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
@@ -63,6 +55,20 @@ return packer.startup(function(use)
   use { "akinsho/bufferline.nvim", commit = "83bf4dc7bff642e145c8b4547aa596803a8b4dc4" }
   use { "nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
   use { "akinsho/toggleterm.nvim", commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda" }
+
+  -- Colorschemes
+  use { "sainnhe/gruvbox-material", commit = "2807579bd0a9981575dbb518aa65d3206f04ea02" }
+
+  -- Utils
+  use { "windwp/nvim-autopairs", commit = "4fc96c8f3df89b6d23e5092d31c866c53a346347" } -- autopairs, integrates with both cmp and treesitter
+  use { "numToStr/Comment.nvim", commit = "97a188a98b5a3a6f9b1b850799ac078faa17ab67" }
+  use { "ahmedkhalf/project.nvim", commit = "628de7e433dd503e782831fe150bb750e56e55d6" }
+  use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
+  use {
+    "kevinhwang91/nvim-ufo",
+    requires = "kevinhwang91/promise-async",
+    commit = "5da70eb121a890df8a5b25e6cc30d88665af97b8"
+  }
 
   -- Completions
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- the completion plugin
@@ -91,10 +97,16 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
   }
-  use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "32d9627123321db65a4f158b72b757bcaef1a3f4" }
+  use {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    commit = "32d9627123321db65a4f158b72b757bcaef1a3f4",
+  }
 
   -- Git
-  use { "lewis6991/gitsigns.nvim", commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f" }
+  use {
+    "lewis6991/gitsigns.nvim",
+    commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f",
+  }
 
 
   -- Automatically set up your configuration after cloning packer.nvim
@@ -103,4 +115,3 @@ return packer.startup(function(use)
     require("packer").sync()
   end
 end)
-
