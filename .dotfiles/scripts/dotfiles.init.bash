e61384e3a00da611ef110b125d7fb854587427e9
Ben Elan <belan@esri.com>
2022-11-17 19:59:52 -0600

    chore(dotfiles.init): cleanup and add more comments

--- .dotfiles/scripts/dotfiles.init.bash
+++ .dotfiles/scripts/dotfiles.init.bash
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# This script sets up my dotfiles in a bare git repo
+# so everything is tracked and under version control.
+# I used symlinks for a while but they can be a pain.
+# There are `dot` aliases to manage the bare repo in
+# ~/.dotfiles/sh/aliases/git.alias.sh
+
 # any current, conflicting dotfiles will be moved here
 BACKUP_DIR="$HOME/.dotfiles-backup/"
 
-# Check if there is an ssh key that _might_ be for github
-# Don't use SSH to clone if there is no SSH key
+# Don't use SSH to clone if there is no SSH key on the machine
 if find ~/.ssh -type f -name 'id_rsa.pub' -o -name 'id_ed25519.pub' -o -name 'id_ecdsa.pub' |
   wc -l | xargs test 0 -eq; then
   GIT_URL=https://github.com/benelan/dotfiles
@@ -18,30 +23,28 @@ if /usr/bin/git --git-dir="$HOME/.git/" --work-tree="$HOME" checkout; then
   echo "Checked out dotfiles"
 else
   echo "Backing up pre-existing dotfiles"
-  # get the list of files that need to be backed up
+  # Get the list of files that need to be backed up
   files=$(/usr/bin/git --git-dir="$HOME/.git/" --work-tree="$HOME" checkout 2>&1 | grep -e "^\s")
  
-  # get names of the directories by
-  # removing everything after the last "/" from file paths
-  # files in ~ will end up being blank lines, which need to be stripped
-  folders=$(echo "$files" | awk 'BEGIN{FS=OFS="/"} {NF--} 1' | sed '/^[[:blank:]]*$/d')
+  # Get the directories by removing everything after the last "/" from file paths
+  # Files in $HOME will end up being blank lines, which need to be stripped
+  dirs=$(echo "$files" | awk 'BEGIN{FS=OFS="/"} {NF--} 1' | sed '/^[[:blank:]]*$/d')
   
-  # finally create the directories in the BACKUP_DIR
+  # Create the directories in $BACKUP_DIR
   mkdir -p "$BACKUP_DIR"
-  echo "$folders" | xargs -I{} mkdir -p "$BACKUP_DIR{}"
-  # move the files to the new directories
+  echo "$dirs" | xargs -I{} mkdir -p "$BACKUP_DIR{}"
+  # Move the conflicting files to the new directories
   echo "$files" | xargs -I{} mv {} "$BACKUP_DIR/{}"
-
-  # alternatively you can use rsync to backup the files
-  # rsync --remove-source-files -avh \
-  #     $(dotfiles checkout 2>&1 | grep -E "\s+\." | awk {'print $1'}) "$BACKUP_DIR"
-
-  # checkout the dotfiles now that there are no conflicts
+  
+  # Checkout the dotfiles now that there are no conflicts
+  # To undo this script, move the files from $BACKUP_DIR to $HOME
+  # $ mv ~/.dotfiles-backup/* ~/
   /usr/bin/git --git-dir="$HOME/.git/" --work-tree="$HOME" checkout 
 fi
 
-# prevents showing everything in ~
-# to track new files: "dot add ~/.npmrc"
+# Prevents showing everything in $HOME when using status/diff
+# Dotfiles need to be manually added for them to show up
+# $ dot add ~/.npmrc
 /usr/bin/git --git-dir="$HOME/.git/" --work-tree="$HOME" config status.showUntrackedFiles no
 
 # Install fzf
@@ -58,7 +61,6 @@ unset BACKUP_DIR
 /usr/bin/git --git-dir="$HOME/.git/" --work-tree="$HOME" update-index --assume-unchanged "$HOME/LICENSE.md"
 /usr/bin/git --git-dir="$HOME/.git/" --work-tree="$HOME" update-index --assume-unchanged "$HOME/README.md"
 rm -f "$HOME/README.md" "$HOME/LICENSE.md"
-rm -f "$HOME/LICENSE.md"
 
-# make the bins executable
-[ -d ~/.bin/ ] && chmod +x ~/.bin/*;
+# Make the bins executable
+[ -d ~/.bin/ ] && chmod +x ~/.bin/*
