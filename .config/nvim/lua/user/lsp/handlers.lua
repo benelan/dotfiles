77379017f879a8726cd34bc6099a5af7d2196a75
Ben Elan <belan@esri.com>
2022-11-26 17:14:27 -0800

    chore(nvim): fix linting warning

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -89,10 +89,8 @@ M.on_attach = function(client, bufnr)
   end
 
   lsp_keymaps(bufnr)
-  local status_ok, illuminate = pcall(require, "illuminate")
-  if not status_ok then
-    return
-  end
+  local illuminate_status_ok, illuminate = pcall(require, "illuminate")
+  if not illuminate_status_ok then return end
   illuminate.on_attach(client)
 end
 
