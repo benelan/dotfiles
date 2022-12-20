aa3cf30bae62571c9e49264d74d2b8a6c8f12138
Ben Elan <belan@esri.com>
2022-12-19 22:17:24 -0800

    chore(nvim): change alpha header

--- .config/nvim/lua/user/setups/alpha.lua
+++ .config/nvim/lua/user/setups/alpha.lua
@@ -2,14 +2,19 @@ local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then return end
 
 local dashboard = require "alpha.themes.dashboard"
+
 dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+  [[    $$$$$\                         $$\                  $$$$$$\                  $$\            ]],
+  [[    \__$$ |                        \__|                $$  __$$\                 $$ |           ]],
+  [[       $$ | $$$$$$\  $$$$$$\$$$$\  $$\ $$$$$$$\        $$ /  \__| $$$$$$\   $$$$$$$ | $$$$$$\   ]],
+  [[       $$ | \____$$\ $$  _$$  _$$\ $$ |$$  __$$\       $$ |      $$  __$$\ $$  __$$ |$$  __$$\  ]],
+  [[ $$\   $$ | $$$$$$$ |$$ / $$ / $$ |$$ |$$ |  $$ |      $$ |      $$ /  $$ |$$ /  $$ |$$$$$$$$ | ]],
+  [[ $$ |  $$ |$$  __$$ |$$ | $$ | $$ |$$ |$$ |  $$ |      $$ |  $$\ $$ |  $$ |$$ |  $$ |$$   ____| ]],
+  [[ \$$$$$$  |\$$$$$$$ |$$ | $$ | $$ |$$ |$$ |  $$ |      \$$$$$$  |\$$$$$$  |\$$$$$$$ |\$$$$$$$\  ]],
+  [[ \______/  \_______|\__| \__| \__|\__|\__|  \__|       \______/  \______/  \_______| \_______|  ]],
+
 }
+
 dashboard.section.buttons.val = {
   dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
   dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
@@ -21,15 +26,9 @@ dashboard.section.buttons.val = {
   dashboard.button("c", " " .. " Config", ":e $MYVIMRC<CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
-local function footer()
-  return "ben@jamin"
-end
 
-dashboard.section.footer.val = footer()
-
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
-
+dashboard.section.footer.val = "ben@jamin"
+dashboard.section.header.opts.hl = "Green"
+dashboard.section.footer.opts.hl = "Blue"
 dashboard.opts.opts.noautocmd = true
 alpha.setup(dashboard.opts)
