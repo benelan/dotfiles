6835bb19fea1e7c59a7448630c0b74627934e88c
Ben Elan <belan@esri.com>
2022-12-21 04:54:37 -0800

    feat(nvim): add LSP  inlay hints functionality

---
+++ .config/nvim/lua/user/setups/lsp/inlays.lua
@@ -0,0 +1,40 @@
+local status_ok, inlay_hints = pcall(require, "inlay-hints")
+if not status_ok then return end
+
+inlay_hints.setup({
+  -- only_current_line = true,
+  hints = {
+    parameter = {
+      show = true,
+      highlight = "whitespace",
+    },
+    type = {
+      show = true,
+      highlight = "Whitespace",
+    },
+  },
+  eol = {
+    right_align = true
+  },
+  parameter = {
+    separator = ", ",
+    format = function(hints)
+      return string.format(" <- (%s)", hints)
+    end,
+  },
+
+  type = {
+    separator = ", ",
+    format = function(hints)
+      return string.format(" => %s", hints)
+    end,
+  },
+})
+
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   group = vim.api.nvim_create_augroup("my-inlay-hints", {}),
+--   callback = function(args)
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     require("inlay-hints").on_attach(client, args.buf)
+--   end,
+-- })
