0c1ef2c4456bcaf350783781fc40b20d4febc435
Ben Elan <belan@esri.com>
2022-12-23 23:45:58 -0800

    chore(nvim): move colorscheme to globals file

--- .config/nvim/lua/user/globals.lua
+++ .config/nvim/lua/user/globals.lua
@@ -28,6 +28,7 @@ end, {
   'ruby_provider'
 })
 
+-- neovide options
 if vim.fn.exists("g:neovide") == 1 then
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_scroll_animation_length = 0.1
@@ -38,9 +39,17 @@ if vim.fn.exists("g:neovide") == 1 then
   if vim.fn.has("wsl") then vim.opt.guifont = "SauceCodePro_NF:h10" end
 end
 
-
+-- markdown fenced code syntax highlighting
 vim.g.markdown_fenced_languages = {
   'css', 'scss', 'sass', 'html', 'xml', 'json',
   'ts=typescript', 'tsx=typescriptreact',
   'js=javascript', 'jsx=javascriptreact'
 }
+
+-- theme options
+vim.g.gruvbox_material_background = "medium"
+vim.g.gruvbox_material_foreground = "material"
+vim.g.gruvbox_material_enable_italic = true
+vim.g.gruvbox_material_ui_contrast = false
+local colorscheme = "gruvbox-material"
+vim.cmd("colorscheme " .. colorscheme)
