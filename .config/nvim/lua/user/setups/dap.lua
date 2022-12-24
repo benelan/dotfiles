5e2a70b5203db4ea9f4ab545777ed5fe897852c7
Ben Elan <belan@esri.com>
2022-12-23 23:45:07 -0800

    chore(nvim): second try at using dap

--- .config/nvim/lua/user/setups/dap.lua
+++ .config/nvim/lua/user/setups/dap.lua
@@ -1,5 +1,5 @@
 local status_ok, dap = pcall(require, "dap")
-local ui_status_ok, dapui = pcall(require, "ui")
+local ui_status_ok, dapui = pcall(require, "dapui")
 local vt_status_ok, virtual_text = pcall(require, "nvim-dap-virtual-text")
 
 if not status_ok or not ui_status_ok or not vt_status_ok then
@@ -12,6 +12,126 @@ end
 --   https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
 -- Debug Adapter protocol:
 --   https://microsoft.github.io/debug-adapter-protocol/
+
+
+-- Adapers
+----------
+-- Node
+dap.adapters.node2 = {
+  type = 'executable';
+  command = 'node',
+  args = { vim.fn.stdpath "data" .. '/mason/packages/node-debug2-adapter/out/src/nodeDebugAdapter.js' };
+}
+
+-- Chrome
+dap.adapters.chrome = {
+  type = 'executable',
+  command = 'node',
+  args = { vim.fn.stdpath "data" .. '/mason/packages/chrome-debug-adapter/out/src/chromeDebugAdapter.js' };
+}
+
+-- Language configuration
+-------------------------
+dap.configurations.javascript = {
+  {
+    name = "Launch",
+    type = "node2",
+    request = "launch",
+    program = "${workspaceFolder}/${file}",
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+    console = "integratedTerminal",
+  },
+  {
+    -- For this to work you need to make sure
+    -- the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require 'dap.utils'.pick_process,
+  },
+  {
+    name = "Chrome",
+    type = 'chrome',
+    request = 'attach',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    port = 9222,
+    webRoot = '${workspaceFolder}'
+  }
+}
+
+dap.configurations.typescript = {
+  {
+    name = "Launch",
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure
+    -- the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require 'dap.utils'.pick_process,
+  },
+  {
+    name = "Chrome",
+    type = 'chrome',
+    request = 'attach',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    port = 9222,
+    webRoot = '${workspaceFolder}'
+  },
+  {
+    type = "node2",
+    request = "launch",
+    name = "Debug all Stencil tests",
+    cwd = "${workspaceFolder}",
+    program = "${workspaceFolder}/node_modules/.bin/stencil",
+    args = { "test", "--spec", "--e2e", "--devtools" },
+    console = "integratedTerminal",
+    internalConsoleOptions = "neverOpen"
+
+  }
+}
+
+dap.configurations.javascriptreact = {
+  {
+    type = 'chrome',
+    request = 'attach',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    port = 9222,
+    webRoot = '${workspaceFolder}'
+  }
+}
+
+dap.configurations.typescriptreact = {
+  {
+    type = 'chrome',
+    request = 'attach',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    port = 9222,
+    webRoot = '${workspaceFolder}'
+  }
+}
 
 -----------------------------------------------------------------------------
 -- DAP Virtual Text
@@ -139,108 +259,3 @@ vim.g.dap_virtual_text = true
 
 vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define('DapStopped', { text = '⭐️', texthl = '', linehl = '', numhl = '' })
-
-
--- Adapers
-----------
--- Node
-dap.adapters.node2 = {
-  type = 'executable';
-  command = 'node',
-  args = { vim.fn.stdpath "data" .. '/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' };
-}
-
--- Chrome
-dap.adapters.chrome = {
-  type = 'executable',
-  command = 'node',
-  args = { vim.fn.stdpath "data" .. '/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js' };
-}
-
--- Language configuration
--------------------------
-dap.configurations.javascript = {
-  {
-    type = 'node2';
-    request = 'launch';
-    program = '${file}';
-    cwd = vim.fn.getcwd();
-    sourceMaps = true;
-    protocol = 'inspector';
-    console = 'integratedTerminal';
-  },
-  {
-    -- For this to work you need to make sure
-    -- the node process is started with the `--inspect` flag.
-    name = 'Attach to process',
-    type = 'node2',
-    request = 'attach',
-    processId = require 'dap.utils'.pick_process,
-  },
-  {
-    type = 'chrome',
-    request = 'attach',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    port = 9222,
-    webRoot = '${workspaceFolder}'
-  }
-}
-
-dap.configurations.typescript = {
-  {
-    type = 'node2';
-    request = 'launch';
-    program = '${file}';
-    cwd = vim.fn.getcwd();
-    sourceMaps = true;
-    protocol = 'inspector';
-    console = 'integratedTerminal';
-  },
-  {
-    -- For this to work you need to make sure
-    -- the node process is started with the `--inspect` flag.
-    name = 'Attach to process',
-    type = 'node2',
-    request = 'attach',
-    processId = require 'dap.utils'.pick_process,
-  },
-  {
-    type = 'chrome',
-    request = 'attach',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    port = 9222,
-    webRoot = '${workspaceFolder}'
-  }
-}
-
-dap.configurations.javascriptreact = {
-  {
-    type = 'chrome',
-    request = 'attach',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    port = 9222,
-    webRoot = '${workspaceFolder}'
-  }
-}
-
-dap.configurations.typescriptreact = {
-  {
-    type = 'chrome',
-    request = 'attach',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    port = 9222,
-    webRoot = '${workspaceFolder}'
-  }
-}
