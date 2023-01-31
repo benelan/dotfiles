31658ebb0bdd20095491d47be170faaad9d1a88e
Ben Elan <ben0elan@gmail.com>
2023-01-31 14:00:43 -0800

    fix(nvim): rework cmp keymaps to fix TAB key behavior

--- .config/nvim/lua/user/setups/cmp.lua
+++ .config/nvim/lua/user/setups/cmp.lua
@@ -5,6 +5,12 @@ if not cmp_status_ok or not snip_status_ok or not res_status_okay then
   return
 end
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 local vscode_snips = require "luasnip/loaders/from_vscode"
 vscode_snips.lazy_load() -- load plugin snippets
 vscode_snips.lazy_load { -- load personal snippets
@@ -17,12 +23,11 @@ cmp.setup {
       require("luasnip").lsp_expand(args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert {
+  mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
     ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<C-h>"] = cmp.mapping.abort(),
     ["<C-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
@@ -34,37 +39,54 @@ cmp.setup {
       },
       { "i", "c" }
     ),
-    ["<M-y>"] = cmp.mapping(
+    ["<C-x>"] = cmp.mapping(
       cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Replace,
         select = false,
       },
       { "i", "c" }
     ),
-    ["<C-l>"] = cmp.mapping(
-      cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-      { "i", "c" }
-    ),
-    ["<C-i>"] = cmp.mapping {
-      i = cmp.mapping.complete(),
-      c = function(
-        _ --[[fallback]]
-      )
-        if cmp.visible() then
-          if not cmp.confirm { select = true } then
-            return
-          end
-        else
-          cmp.complete()
-        end
-      end,
-    },
-    -- Accept currently selected item. If none selected, `select` first item.
-    -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = false },
+    ["<C-n>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s", "c" }),
+    ["<C-p>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item { behavior = cmp.SelectBehavior.Insert }
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s", "c" }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif ls.expandable() then
+        ls.expand()
+      elseif ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      -- elseif has_words_before() then
+      --   cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s", "c" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif ls.jumpable(-1) then
+        ls.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s", "c" }),
   },
   formatting = {
     fields = { "kind", "abbr", "menu" },
@@ -81,7 +103,7 @@ cmp.setup {
       vim_item.menu = ({
         buffer = " [BUF] ",
         nvim_lsp = " [LSP] ",
-        nvim_lsp_signature_help = "[ LSP] ",
+        nvim_lsp_signature_help = " [LSP] ",
         nvim_lua = " [API] ",
         path = " [PATH] ",
         luasnip = "[SNIP] ",
@@ -108,7 +130,7 @@ cmp.setup {
       cmp.config.compare.order,
     },
   },
-  confirm_opts = { behavior = cmp.ConfirmBehavior.Replace, select = false },
+  -- confirm_opts = { behavior = cmp.ConfirmBehavior.Replace, select = false },
   window = {
     -- completion = cmp.config.window.bordered({scrollbar = false}),
     -- documentation = cmp.config.window.bordered({scrollbar = false}),
@@ -126,13 +148,17 @@ cmp.setup {
 -- Use buffer source for `/` and `?`
 cmp.setup.cmdline({ "/", "?" }, {
   mapping = cmp.mapping.preset.cmdline(),
-  sources = { { name = "buffer" } },
+  sources = {
+    { name = "buffer", opts = { keyword_pattern = [=[[^[:blank:]].*]=] } },
+  },
 })
 
 -- Use cmdline & path source for ':'
 cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, { { name = "cmdline" } }),
 })
 
 local opts = { noremap = true, silent = true }
