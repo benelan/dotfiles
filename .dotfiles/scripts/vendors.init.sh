563b72410b3ff539907b64392c85af92dcf1b92e
Ben Elan <ben0elan@gmail.com>
2023-02-03 23:46:34 -0800

    build: update vendors and vendor script

--- .dotfiles/scripts/vendors.init.sh
+++ .dotfiles/scripts/vendors.init.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-NEOVIM_REPO="$HOME/.dotfiles/vendor/neovim"
-FZF_REPO="$HOME/.dotfiles/vendor/fzf"
+VENDOR_PATH="$HOME/.dotfiles/vendor"
 
-/usr/bin/git --git-dir="$HOME"/.git/ --work-tree="$HOME" "$@" submodule update --init --recursive
-/usr/bin/git --git-dir="$HOME"/.git/ --work-tree="$HOME" "$@" submodule foreach git pull
+/usr/bin/git --git-dir="$HOME"/.git/ --work-tree="$HOME" submodule update --init --recursive
+/usr/bin/git --git-dir="$HOME"/.git/ --work-tree="$HOME" pull --recurse-submodules
 
-[ -d "$FZF_REPO" ] && "$FZF_REPO/install" \
-    --bin --key-bindings --completion --no-update-rc
-
-if [ -d "$NEOVIM_REPO" ]; then
-    cd "$NEOVIM_REPO" || return
-    make CMAKE_BUILD_TYPE=Release
+if [ -d "$VENDOR_PATH/fzf" ]; then
+    cd "$VENDOR_PATH/fzf" || return
+    git fetch --tags --all
+    latestTag="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
+    git checkout "$latestTag"
     sudo make install
-    chmod +x "$NEOVIM_REPO/build/bin/nvim"
+fi
+
+if [ -d "$VENDOR_PATH/neovim" ]; then
+    cd "$VENDOR_PATH/neovim" || return
+    git checkout master
+    git pull
+    make CMAKE_BUILD_TYPE=Release
+    # make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.dotfiles/cache/neovim"
+    sudo make install
+    chmod +x "$VENDOR_PATH/neovim/build/bin/nvim"
 fi
