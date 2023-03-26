00c5374ba302b01f7d122207a9b3ab372b5d2030
Ben Elan <no-reply@benelan.dev>
2023-03-25 19:40:06 -0700

    fix(nvim): add back mason and null-ls configs

---
+++ .config/nvim/lua/user/plugins/mason.lua
@@ -0,0 +1,76 @@
+return {
+  "williamboman/mason.nvim", -- language server installer/manager
+  dependencies = {
+    { "williamboman/mason-lspconfig.nvim" }, -- integrates mason and lspconfig
+    { "neovim/nvim-lspconfig" }, -- neovim's LSP implementation
+  },
+  config = function()
+    local mason_status_ok, mason = pcall(require, "mason")
+    local mason_lspconfig_status_ok, mason_lspconfig =
+      pcall(require, "mason-lspconfig")
+    local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+    if
+      not lspconfig_status_ok
+      or not mason_lspconfig_status_ok
+      or not mason_status_ok
+    then
+      return
+    end
+
+    local servers = {
+      "bashls",
+      "cssls",
+      "docker_compose_language_service",
+      "dockerls",
+      "eslint",
+      -- "gopls",
+      -- "graphql",
+      "html",
+      "jsonls",
+      "lua_ls",
+      "marksman",
+      -- "pyright",
+      -- "rust_analyzer",
+      -- "sqlls",
+      "svelte",
+      "taplo",
+      "tailwindcss",
+      "tsserver",
+      "vimls",
+      "volar",
+      "yamlls",
+      "zk",
+    }
+
+    local settings = {
+      ui = { border = "none" },
+      log_level = vim.log.levels.INFO,
+      max_concurrent_installers = 4,
+    }
+
+    mason.setup(settings)
+    mason_lspconfig.setup {
+      ensure_installed = servers,
+      automatic_installation = true,
+    }
+
+    for _, server in pairs(servers) do
+      local opts = {
+        on_attach = require("user.lsp.handlers").on_attach,
+        capabilities = require("user.lsp.handlers").capabilities,
+      }
+
+      server = vim.split(server, "@")[1]
+
+      local require_ok, conf_opts =
+        pcall(require, "user.lsp.servers." .. server)
+      if require_ok then
+        opts = vim.tbl_deep_extend("force", conf_opts, opts)
+      end
+
+      lspconfig[server].setup(opts)
+    end
+  end,
+
+  require("user.lsp.handlers").setup(),
+}
---
+++ .config/nvim/lua/user/plugins/null-ls.lua
@@ -0,0 +1,65 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim", -- integrates formatters and linters
+  dependencies = {
+    { "nvim-lua/plenary.nvim" }, -- neovim utils
+    { "neovim/nvim-lspconfig" }, -- neovim's LSP implementation
+  },
+  config = function()
+    local status_ok, null_ls = pcall(require, "null-ls")
+    if not status_ok then
+      return
+    end
+
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+    local code_actions = null_ls.builtins.code_actions
+    local hover = null_ls.builtins.hover
+
+    local quiet_diagnostics = { virtual_text = false, signs = false }
+
+    -- Install with Mason if you don't have all of these linters/formatters
+    -- :MasonInstall actionlint cspell jq shellcheck...
+    null_ls.setup {
+      debug = false,
+      fallback_severity = vim.diagnostic.severity.WARN,
+      sources = {
+        code_actions.gitrebase,
+        code_actions.gitsigns,
+        code_actions.proselint,
+        code_actions.shellcheck,
+        diagnostics.actionlint.with {
+          runtime_condition = function()
+            return vim.api
+              .nvim_buf_get_name(vim.api.nvim_get_current_buf())
+              :match "github/workflows" ~= nil
+          end,
+        },
+        diagnostics.markdownlint.with {
+          extra_args = { "--disable", "MD024", "MD013", "MD041", "MD033" },
+          prefer_local = "node_modules/.bin",
+        },
+        diagnostics.proselint.with { diagnostic_config = quiet_diagnostics },
+        diagnostics.stylelint.with { prefer_local = "node_modules/.bin" },
+        diagnostics.write_good.with { diagnostic_config = quiet_diagnostics },
+        formatting.jq.with { extra_filetypes = { "jsonc", "json5" } },
+        formatting.markdown_toc,
+        formatting.markdownlint.with { prefer_local = "node_modules/.bin" },
+        formatting.prettier.with {
+          disabled_filetypes = { "json", "jsonc", "json5" },
+          prefer_local = "node_modules/.bin",
+        },
+        formatting.lua_format,
+        -- Reminder: be careful with shellharden if you (ab)use expansion
+        -- it can break your code w/o warning when you format()
+        -- formatting.shellharden,
+        formatting.shfmt.with { extra_args = { "-i", "4", "-ci" } },
+        formatting.stylelint.with { prefer_local = "node_modules/.bin" },
+        formatting.stylua,
+        formatting.trim_whitespace,
+        hover.dictionary,
+        hover.printenv,
+      },
+    }
+  end,
+}
