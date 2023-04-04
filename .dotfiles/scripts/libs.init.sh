3afed88eaaaccc6f0cc7405ece7eb9c5db5081d8
Ben Elan <no-reply@benelan.dev>
2023-04-04 12:31:44 -0700

    build: update libs and swap build order

--- .dotfiles/scripts/libs.init.sh
+++ .dotfiles/scripts/libs.init.sh
@@ -7,6 +7,16 @@ NEOVIM_PATH="$LIB_PATH/neovim"
 
 /usr/bin/git --git-dir="$HOME"/.git/ --work-tree="$HOME" submodule update --init --recursive
 
+if [ -d "$NEOVIM_PATH" ]; then
+    cd "$NEOVIM_PATH" || exit 1
+    git fetch --all
+    git pull origin master
+    git checkout nightly
+    sudo make CMAKE_BUILD_TYPE=Release
+    sudo make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.dotfiles/cache"
+    sudo make install
+fi
+
 if [ -d "$FZF_PATH" ]; then
     cd "$FZF_PATH" || exit 1
     git fetch --tags --all
@@ -16,16 +26,6 @@ if [ -d "$FZF_PATH" ]; then
     make
     make install
     chmod +x "$FZF_PATH/bin/fzf"
-fi
-
-if [ -d "$NEOVIM_PATH" ]; then
-    cd "$NEOVIM_PATH" || exit 1
-    git fetch --all
-    git pull origin master
-    git checkout nightly
-    sudo make CMAKE_BUILD_TYPE=Release
-    sudo make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.dotfiles/cache"
-    sudo make install
 fi
 
 unset LIB_PATH FZF_PATH NEOVIM_PATH
