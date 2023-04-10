dfb9d0a2dd8ed248c02bace99f55aa33f1e9bf12
Ben Elan <no-reply@benelan.dev>
2023-04-10 14:05:56 -0700

    feat(nvim): add diffview keymaps for resolving conflicts

--- .config/nvim/lua/user/plugins/diffview.lua
+++ .config/nvim/lua/user/plugins/diffview.lua
@@ -58,6 +58,44 @@ return {
             require("diffview.actions").conflict_choose "none",
             { desc = "Delete the conflict region" },
           },
+          {
+            "n",
+            "<leader>cO",
+            require("diffview.actions").conflict_choose_all "ours",
+            {
+              desc = "Choose the OURS version of a conflict for the whole file",
+            },
+          },
+          {
+            "n",
+            "<leader>cT",
+            require("diffview.actions").conflict_choose_all "theirs",
+            {
+              desc = "Choose the THEIRS version of a conflict for the whole file",
+            },
+          },
+          {
+            "n",
+            "<leader>cB",
+            require("diffview.actions").conflict_choose_all "base",
+            {
+              desc = "Choose the BASE version of a conflict for the whole file",
+            },
+          },
+          {
+            "n",
+            "<leader>cA",
+            require("diffview.actions").conflict_choose_all "all",
+            {
+              desc = "Choose all the versions of a conflict for the whole file",
+            },
+          },
+          {
+            "n",
+            "<leader>cX",
+            require("diffview.actions").conflict_choose_all "none",
+            { desc = "Delete the conflict region for the whole file" },
+          },
         },
         file_panel = {
           {
@@ -71,6 +109,74 @@ return {
             "<leader>e",
             require("diffview.actions").toggle_files,
             { desc = "Toggle the file panel" },
+          },
+          {
+            "n",
+            "<leader>co",
+            require("diffview.actions").conflict_choose "ours",
+            { desc = "Choose the OURS version of a conflict" },
+          },
+          {
+            "n",
+            "<leader>ct",
+            require("diffview.actions").conflict_choose "theirs",
+            { desc = "Choose the THEIRS version of a conflict" },
+          },
+          {
+            "n",
+            "<leader>cb",
+            require("diffview.actions").conflict_choose "base",
+            { desc = "Choose the BASE version of a conflict" },
+          },
+          {
+            "n",
+            "<leader>ca",
+            require("diffview.actions").conflict_choose "all",
+            { desc = "Choose all the versions of a conflict" },
+          },
+          {
+            "n",
+            "<leader>cx",
+            require("diffview.actions").conflict_choose "none",
+            { desc = "Delete the conflict region" },
+          },
+          {
+            "n",
+            "<leader>cO",
+            require("diffview.actions").conflict_choose_all "ours",
+            {
+              desc = "Choose the OURS version of a conflict for the whole file",
+            },
+          },
+          {
+            "n",
+            "<leader>cT",
+            require("diffview.actions").conflict_choose_all "theirs",
+            {
+              desc = "Choose the THEIRS version of a conflict for the whole file",
+            },
+          },
+          {
+            "n",
+            "<leader>cB",
+            require("diffview.actions").conflict_choose_all "base",
+            {
+              desc = "Choose the BASE version of a conflict for the whole file",
+            },
+          },
+          {
+            "n",
+            "<leader>cA",
+            require("diffview.actions").conflict_choose_all "all",
+            {
+              desc = "Choose all the versions of a conflict for the whole file",
+            },
+          },
+          {
+            "n",
+            "<leader>cX",
+            require("diffview.actions").conflict_choose_all "none",
+            { desc = "Delete the conflict region for the whole file" },
           },
         },
         file_history_panel = {
