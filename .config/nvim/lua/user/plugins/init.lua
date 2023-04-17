8c929467a54ae64b6b14a1483fcb0dd87b87f2f5
Ben Elan <no-reply@benelan.dev>
2023-04-17 01:53:49 -0700

    feat(nvim): add leap plugin

--- .config/nvim/lua/user/plugins/init.lua
+++ .config/nvim/lua/user/plugins/init.lua
@@ -110,4 +110,42 @@ return {
       },
     },
   },
+  {
+    "ggandor/leap.nvim",
+    -- enabled = false,
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    config = function(_, opts)
+      local leap = require "leap"
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+    end,
+    dependencies = {
+      {
+        "ggandor/leap-spooky.nvim",
+        config = true,
+        keys = {
+          { "am", mode = { "o" }, desc = "Leap magnet" },
+          { "im", mode = { "o" }, desc = "Leap magnet" },
+          { "mm", mode = { "o" }, desc = "Leap magnet line" },
+          { "aM", mode = { "o" }, desc = "Leap magnet (windows)" },
+          { "iM", mode = { "o" }, desc = "Leap magnet (windows)" },
+          { "MM", mode = { "o" }, desc = "Leap magnet line (windows)" },
+          { "ar", mode = { "o" }, desc = "Leap remote" },
+          { "ir", mode = { "o" }, desc = "Leap remote" },
+          { "rr", mode = { "o" }, desc = "Leap remote line" },
+          { "aR", mode = { "o" }, desc = "Leap remote (windows)" },
+          { "iR", mode = { "o" }, desc = "Leap remote (windows)" },
+          { "RR", mode = { "o" }, desc = "Leap remote line (windows)" },
+        },
+      },
+    },
+  },
 }
