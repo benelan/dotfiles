20bccef2fdc361591daa48278b66e17fd19aee5f
Ben Elan <no-reply@benelan.dev>
2023-04-27 21:54:37 -0700

    build(nvim): bump plugins and add obsession/exchange

--- .dotfiles/scripts/vim.init.sh
+++ .dotfiles/scripts/vim.init.sh
@@ -164,7 +164,7 @@ install_undotree() {
 install_commentary
 install_eunuch
 install_fugitive
-# install_obession
+install_obession
 install_repeat
 install_rsi
 install_surround
@@ -173,7 +173,7 @@ install_surround
 install_closer
 # install_codi
 install_easy_align
-# install_exchange
+install_exchange
 # install_grepper
 install_gruvbox_material
 install_rooter
