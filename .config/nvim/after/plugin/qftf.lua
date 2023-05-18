1659775e607fbd30fea1c4f923503b44239fdba1
Ben Elan <no-reply@benelan.dev>
2023-05-18 15:52:36 -0700

    chore(nvim): tweak quickfix window format

--- .config/nvim/after/plugin/qftf.lua
+++ .config/nvim/after/plugin/qftf.lua
@@ -2,15 +2,15 @@
 function _G.qftf(info)
   local items
   local ret = {}
+  local limit = 30
+  local validFmt = "%s │%8s : %-7s│%s %s"
+
   if info.quickfix == 1 then
     items = vim.fn.getqflist({ id = info.id, items = 0 }).items
   else
     items = vim.fn.getloclist(info.winid, { id = info.id, items = 0 }).items
   end
-  local limit = 30
-  local fnameFmt1, fnameFmt2 =
-    "%-" .. limit .. "s", "…%." .. (limit - 1) .. "s"
-  local validFmt = "%s │%5d:%-3d│%s %s"
+
   for i = info.start_idx, info.end_idx do
     local e = items[i]
     local fname = ""
@@ -23,17 +23,37 @@ function _G.qftf(info)
         else
           fname = fname:gsub("^" .. vim.env.HOME, "~")
         end
+
         -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
         if #fname <= limit then
-          fname = fnameFmt1:format(fname)
+          fname = ("%-" .. limit .. "s"):format(fname)
         else
-          fname = fnameFmt2:format(fname:sub(1 - limit))
+          fname = ("… %." .. (limit - 2) .. "s"):format(fname:sub(2 - limit))
         end
       end
+
       local lnum = e.lnum > 99999 and -1 or e.lnum
+      local end_lnum = e.end_lnum > 99999 and -1 or e.end_lnum
+      local lnum_diff = end_lnum - lnum
+      local lnums = lnum_diff == 0 and lnum
+        or lnum .. "+" .. (lnum_diff > 99 and ">C" or lnum_diff)
+
       local col = e.col > 999 and -1 or e.col
-      local qtype = e.type == "" and "" or " " .. e.type:sub(1, 1):upper()
-      str = validFmt:format(fname, lnum, col, qtype, e.text)
+      local end_col = e.end_col > 999 and -1 or e.end_col
+      local cols = col == end_col and col or col .. "-" .. end_col
+
+      local qtype = e.type == "" and ""
+        or " " .. e.type:sub(1, 1):upper() .. ":"
+
+      str = validFmt:format(
+        fname,
+        -- "[" .. lnums .. "]",
+        -- "[" .. cols .. "]",
+        lnums,
+        cols,
+        qtype,
+        e.text
+      )
     else
       str = e.text
     end
--- .config/nvim/syntax/qf.vim
+++ .config/nvim/syntax/qf.vim
@@ -6,10 +6,10 @@ syn match qfFileName /^[^│]*/ nextgroup=qfSeparatorLeft
 syn match qfSeparatorLeft /│/ contained nextgroup=qfLineNr
 syn match qfLineNr /[^│]*/ contained nextgroup=qfSeparatorRight
 syn match qfSeparatorRight '│' contained nextgroup=qfError,qfWarning,qfInfo,qfNote
-syn match qfError / E .*$/ contained
-syn match qfWarning / W .*$/ contained
-syn match qfInfo / I .*$/ contained
-syn match qfNote / [NH] .*$/ contained
+syn match qfError / E: .*$/ contained
+syn match qfWarning / W: .*$/ contained
+syn match qfInfo / I: .*$/ contained
+syn match qfNote / [NH]: .*$/ contained
 
 hi def link qfFileName Directory
 hi def link qfSeparatorLeft Delimiter
