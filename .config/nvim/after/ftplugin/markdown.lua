ae52aacb4ec1c010ea802d47f755cb61fbc99693
Ben Elan <no-reply@benelan.dev>
2023-05-26 17:39:46 -0700

    refactor(nvim): make zk markdown keymaps local to buffer

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,12 +1,17 @@
 vim.wo.conceallevel = 2
 
-keymap("n", "<C-z>", "[s1z=", "Correct latest misspelled word")
-keymap(
-  "i",
-  "<C-z>",
-  "<C-g>u<Esc>[s1z=`]a<C-g>u",
-  "Correct latest misspelled word"
-)
+vim.keymap.set("n", "<M-z>", "[s1z=", {
+  buffer = true,
+  silent = true,
+  noremap = true,
+  desc = "Correct latest misspelled word",
+})
+vim.keymap.set("i", "<M-z>", "<C-g>u<Esc>[s1z=`]a<C-g>u", {
+  buffer = true,
+  silent = true,
+  noremap = true,
+  desc = "Correct latest misspelled word",
+})
 
 local status_ok, zk = pcall(require, "zk.util")
 if not status_ok then
@@ -17,39 +22,57 @@ end
 if zk.notebook_root(vim.fn.expand "%:p") ~= nil then
   -- Create a new note after asking for its title.
   -- This overrides the global `<leader>zn` mapping to create the note in the same directory as the current buffer.
-  keymap(
+  vim.keymap.set(
     "n",
     "<leader>zn",
     "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
-    "New note"
+    { buffer = true, silent = true, noremap = true, desc = "New note" }
   )
   -- Create a new note in the same directory as the current buffer, using the current selection for title.
-  keymap(
+  vim.keymap.set(
     "v",
     "<leader>znt",
     ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>",
-    "New note using selected title"
+    {
+      buffer = true,
+      silent = true,
+      noremap = true,
+      desc = "New note using selected title",
+    }
   )
   -- Create a new note in the same directory as the current buffer, using the current selection for note content and asking for its title.
-  keymap(
+  vim.keymap.set(
     "v",
     "<leader>znc",
     ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
-    "New note using selected content"
+    {
+      buffer = true,
+      silent = true,
+      noremap = true,
+      desc = "New note using selected content",
+    }
   )
-
   -- Open notes linking to the current buffer.
-  keymap("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", "Backlinks")
+  vim.keymap.set(
+    "n",
+    "<leader>zb",
+    "<Cmd>ZkBacklinks<CR>",
+    { buffer = true, silent = true, noremap = true, desc = "Backlinks" }
+  )
   -- Alternative for backlinks using pure LSP and showing the source context.
-  -- keymap('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+  -- vim.keymap.set('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
   -- Open notes linked by the current buffer.
-  keymap("n", "<leader>zl", "<Cmd>ZkLinks<CR>", "Links")
-
+  vim.keymap.set(
+    "n",
+    "<leader>zl",
+    "<Cmd>ZkLinks<CR>",
+    { buffer = true, silent = true, noremap = true, desc = "Links" }
+  )
   -- Open the code actions for a visual selection.
-  keymap(
+  vim.keymap.set(
     "v",
     "<leader>za",
     ":'<,'>lua vim.lsp.buf.range_code_action()<CR>",
-    "Code action"
+    { buffer = true, silent = true, noremap = true, desc = "Code action" }
   )
 end
