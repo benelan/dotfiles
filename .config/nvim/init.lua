080c2f09b13ea61426c93b77cb9097d835c9b026
Ben Elan <no-reply@benelan.dev>
2023-05-30 14:19:49 -0700

    fix(neovide): disable broken gruvbox-material background transparency

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,7 @@ vim.g.maplocalleader = " "
 
 -- neovide options
 if vim.g.neovide then
-  vim.g.neovide_transparency = 0.3
+  vim.g.neovide_transparency = 0.9
   vim.g.neovide_scroll_animation_length = 0
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_size = 0
