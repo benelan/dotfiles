702e9df50e67e8dc8ce8565b739d9a82c5d861ee
Ben Elan <no-reply@benelan.dev>
2023-06-02 23:30:06 -0700

    refactor(git)!: move shell aliases to gitconfig and remove gitalias
    BREAKING CHANGE: the shell aliases for git commands have been removed
    and the commonly used ones were converted to git aliases. gitalias was
    also removed because I haven't been using them and it pollutes tab
    completion.

--- .dotfiles/shell/aliases.sh
+++ .dotfiles/shell/aliases.sh
@@ -260,163 +260,12 @@ is-supported gnome-screensaver-command &&
 # -----------------------------------------------------------------------------
 
 alias g='git'
+alias x="git mux"
+alias glz="lazygit"
 
 # deletes local branches already squash merged into the default branch
 # shellcheck disable=2016,2034,2154
 alias gbprune='TARGET_BRANCH="$(git bdefault)" && git fetch --prune --all && git checkout -q "$TARGET_BRANCH" && git for-each-ref refs/heads/ "--format=%(refname:short)" | grep -v -e main -e master -e develop -e dev | while read -r branch; do mergeBase=$(git merge-base "$TARGET_BRANCH" "$branch") && [[ "$(git cherry "$TARGET_BRANCH" "$(git commit-tree "$(git rev-parse "$branch"\^{tree})" -p "$mergeBase" -m _)")" == "-"* ]] && git branch -D "$branch"; done; unset TARGET_BRANCH mergeBase branch'
-
-# add
-######
-alias ga='git add'
-alias gall='git add --all'
-
-# branch
-#########
-alias gb='git branch'
-alias gbD='git branch --delete --force'
-alias gbuoc='git branch -u origin/$(git symbolic-ref --short HEAD)'
-
-# for-each-ref
-###############
-# https://stackoverflow.com/a/58623139/10362396
-alias gbc="git for-each-ref --format='%(color:cyan)%(authordate:format:%m/%d/%Y %I:%M %p) \
-  %(align:25,left)%(color:yellow)%(authorname)%(end) %(color:reset)%(refname:strip=3)' \
-  --sort=authordate refs/remotes"
-
-# commit
-#########
-alias gc='git commit --verbose'
-alias gca='git commit --verbose --all'
-alias gcm='git commit --verbose -m'
-# Add uncommitted and unstaged changes to the last commit
-alias gcamd='git commit --verbose --amend'
-alias gcamdne='git commit --amend --no-edit'
-
-# checkout
-###########
-alias gco='git checkout'
-alias gcob='git checkout -b'
-alias gcom='git checkout "$(git bdefault)"'
-
-# clone
-########
-alias gcl='git clone'
-
-# diff
-#######
-alias gdf='git diff'
-alias gdfs='git diff --staged'
-alias gdft='git difftool'
-# edit the files changed locally
-alias ge='$EDITOR $(git diff --name-only HEAD)'
-# sync origin's default branch and edit the changed files
-# shellcheck disable=2154
-alias geom='default_branch=$(git bdefault); git fetch; git merge "origin/$default_branch"; $EDITOR $(git diff --name-only HEAD "origin/$default_branch"); unset default_branch'
-# open Diffview.nvim
-egdf() { nvim +"DiffviewOpen $*"; }
-
-# fetch
-########
-alias gf='git fetch --all --prune'
-
-# log
-######
-alias ggr='git log --graph --abbrev-commit --date=relative --pretty=format:'\''%C(bold)%C(blue)%h%Creset%C(auto)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset'\'''
-alias gll='git log --stat --pretty=format:'\''%C(blue)%h %Cgreen%an%Creset %C(yellow)%cd%Creset %C(auto)%d %s%+b%n%Creset'\'''
-alias gg=' git log --graph'
-# show branches with unpushed commits
-# https://stackoverflow.com/questions/39220870/in-git-list-names-of-branches-with-unpushed-commits
-alias glup='git log --branches --not --remotes --no-walk --decorate --oneline'
-# show commits in current branch that aren't merged to the default branch
-alias glum='git log "$(git bdefault)" ^HEAD'
-# show new commits created by the last command, e.g. pull
-alias gnew='git log HEAD@{1}..HEAD@{0}'
-
-# files
-########
-# Show untracked files
-alias glsut='git ls-files . --exclude-standard --others'
-# Show unmerged (conflicted) files
-alias glsum='git diff --name-only --diff-filter=U'
-
-# merge
-########
-alias gm='git merge'
-alias gmm='git merge "$(git bdefault)"'
-alias gmom='git fetch && git merge "origin/$(git bdefault)"'
-alias gmt='git mergetool'
-
-# push
-#######
-alias gp='git push'
-alias gpuoc='git push --set-upstream origin $(git symbolic-ref --short HEAD)'
-
-# pull
-#######
-alias gpl='git pull'
-alias gplum='git pull upstream "$(git bdefault)"'
-
-# rebase
-#########
-alias grb='git rebase'
-alias grbma='GIT_SEQUENCE_EDITOR=: git rebase "$(git bdefault)" -i --autosquash'
-# Rebase with latest remote
-alias gfrom='default_branch="(git bdefault)"; git fetch origin "$default_branch" && git rebase "origin/$default_branch" && git update-ref "refs/heads/$default_branch" "origin/$default_branch"; unset default_branch'
-
-# reset
-########
-alias gr='git reset'
-alias grs='git reset --soft'
-alias grH='git reset --hard'
-alias gpristine='git reset --hard && git clean -dfx'
-
-# status
-#########
-alias gs='git status'
-
-# stash
-########
-alias gst='git stash'
-alias gstl='git stash list'
-alias gstpo='git stash pop'
-# 'stash push' introduced in git v2.13.2
-alias gstpu='git stash push'
-
-# submodules
-#############
-alias gsmu='git submodule update --init --recursive'
-
-# tag
-######
-alias gt='git tag'
-# outputs the tag following the provided commit sha
-# useful for finding the first reproducible version
-# for bugs after using git bisect to get the sha
-# $ gtnext 84a29d4 # => v1.0.0-next.295~8 (8 commits before the tag)
-alias gtnext='git name-rev --tags --name-only'
-
-# worktree
-##########
-alias gw='git worktree'
-alias gwa='git worktree add'
-alias gwr='git worktree remove'
-alias gwl='git worktree list'
-alias gwp='git worktree prune'
-
-alias groot='cd "$(git rev-parse --show-toplevel)"'
-alias ghide='git update-index --assume-unchanged'
-alias gunhide='git update-index --no-assume-unchanged'
-
-# plugins
-##########
-alias gx="git mux"
-alias glz="lazygit"
-# delete multiple branches
-alias fgbd="git branch | fzf --multi | xargs git branch -d"
-# fgcoc - checkout git commit
-alias fgcoc='git log --pretty=oneline --abbrev-commit --reverse | fzf --tac +s +m -e | sed "s/ .*//" | xargs git checkout'
-# get git commit sha
-alias fgcs='git log --color=always --pretty=oneline --abbrev-commit --reverse | fzf --tac +s +m -e --ansi --reverse | sed "s/ .*//"'
 
 # -----------------------------------------------------------------------------
 # Dotfiles
@@ -435,7 +284,11 @@ dot() {
         /usr/bin/git --git-dir="$HOME"/.git/ --work-tree="$HOME" "$@"
     fi
 }
+
 alias d='dot'
+# shellcheck disable=2139
+alias lazydot="lazygit --git-dir='$HOME/.git' --work-tree='$HOME'"
+alias dlz="lazydot"
 
 # creates env vars so git plugins
 # work with the bare dotfiles repo
@@ -447,92 +300,8 @@ edit_dotfiles() {
         --cmd 'let $GIT_DIR = expand("~/.git")'
 }
 
-eddf() { edit_dotfiles +"DiffviewOpen $*"; }
 alias edot="edit_dotfiles +\"if !len(argv()) | execute 'Telescope git_files' | endif\""
-alias envim="edit_dotfiles ~/.config/nvim/init.lua"
 alias G="edit_dotfiles +G +'wincmd o'"
-
-# add
-######
-alias da='dot add'
-# doesn't add untracked files
-alias dall='dot add --update'
-
-# branch
-#########
-alias db='dot branch'
-
-# commit
-#########
-alias dc='dot commit --verbose'
-alias dcm='dot commit --verbose -m'
-alias dcamd='dot commit --amend'
-alias dcamdne='dot commit --amend --no-edit'
-
-# checkout
-###########
-alias dco='dot checkout'
-alias dcob='dot checkout -b'
-alias dcom='dot checkout master'
-
-# diff
-#######
-alias ddf='dot diff'
-alias ddfs='dot diff --staged'
-alias ddft='dot difftool'
-alias de='$EDITOR $(dot diff --name-only)'
-
-# log
-######
-alias dgg='dot log --graph --pretty=format:'\''%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset'\'' --abbrev-commit --date=relative'
-alias dgf='dot log --graph --date=short --pretty=format:'\''%C(auto)%h %Cgreen%an%Creset %Cblue%cd%Creset %C(auto)%d %s'\'''
-alias dgup='dot log --branches --not --remotes --no-walk --decorate --oneline'
-
-# merge
-########
-alias dm='dot merge'
-alias dmm='dot merge "$(dbdefault)"'
-alias dmt='dot mergetool'
-
-# pull
-#######
-alias dpl='dot pull'
-
-# push
-#######
-alias dp='dot push'
-alias dpuoc='dot push --set-upstream origin $(dot symbolic-ref --short HEAD)'
-
-# reset
-########
-alias dr='dot reset'
-alias drH='dot reset --hard'
-
-# stash
-########
-alias dst='dot stash'
-alias dstl='dot stash list'
-alias dstpo='dot stash pop'
-alias dstpu='dot stash push'
-
-# status
-#########
-alias ds='dot status'
-
-# submodules
-#############
-alias dsmu='dot submodule update --init --recursive'
-
-# update-index
-##############
-alias dhide='dot update-index --assume-unchanged'
-alias dunhide='dot update-index --no-assume-unchanged'
-
-# plugins
-##########
-# shellcheck disable=2139
-alias lazydot="lazygit --git-dir='$HOME/.git' --work-tree='$HOME'"
-alias dlz="lazydot"
 
 # -----------------------------------------------------------------------------
 # GitHub
