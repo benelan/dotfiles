5f0e2124e5522c13b5de6d6ee0db1a847381713a
Ben Elan <no-reply@benelan.dev>
2023-07-01 04:41:28 -0700

    chore(script): only downloaded debs for uninstalled apps

--- .dotfiles/scripts/ubuntu.init.sh
+++ .dotfiles/scripts/ubuntu.init.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # shellcheck disable=2015
-set -e
-
-sudo -v
 
 # Install scripts specific to Ubuntu/Debian based operating systems
+
+set -e
+sudo -v
 
 DEPS_DIR="$HOME/.dotfiles/deps"
 CACHE_DIR="$HOME/.dotfiles/cache"
 
-# Install packages for Ubuntu/Debian
+# Install CLI apt packages
 # https://manpages.ubuntu.com/manpages/jammy/man8/apt.8
 install_apt_packages() {
     if [ -f "$DEPS_DIR/apt" ]; then
@@ -19,6 +19,8 @@ install_apt_packages() {
         unset pkg
     fi
 }
+
+# Install GUI apt packages
 install_apt_gui_packages() {
     if [ -f "$DEPS_DIR/apt-gui" ]; then
         while IFS="" read -r pkg || [ -n "$pkg" ]; do
@@ -31,33 +33,39 @@ install_apt_gui_packages() {
 # Install VS Code
 # https://code.visualstudio.com/docs/setup/linux
 install_vscode() {
-    deb="$CACHE_DIR/vscode.deb"
-    curl -ssLo "$deb" \
-        "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-    sudo apt install -y "$deb" || true
-    unset deb
+    if ! is-supported code; then
+        deb="$CACHE_DIR/vscode.deb"
+        curl -ssLo "$deb" \
+            "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+        sudo apt install -y "$deb" || true
+        unset deb
+    fi
 }
 
 # Install ProtonVPN CLI
 # https://protonvpn.com/support/linux-vpn-tool/#debian
 install_protonvpn_cli() {
-    checksum="c409c819eed60985273e94e575fd5dfd8dd34baef3764fc7356b0f23e25a372c"
-    deb="protonvpn-stable-release_1.0.3_all.deb"
-    curl -sSLo "$CACHE_DIR/$deb" \
-        "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/$deb"
-    echo "$checksum $CACHE_DIR/$deb" | sha256sum --check -
-    sudo apt install -y "$CACHE_DIR/$deb" || true
-    unset deb
+    if ! is-supported protonvpn-cli; then
+        checksum="c409c819eed60985273e94e575fd5dfd8dd34baef3764fc7356b0f23e25a372c"
+        deb="protonvpn-stable-release_1.0.3_all.deb"
+        curl -sSLo "$CACHE_DIR/$deb" \
+            "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/$deb"
+        echo "$checksum $CACHE_DIR/$deb" | sha256sum --check -
+        sudo apt install -y "$CACHE_DIR/$deb" || true
+        unset deb
+    fi
 }
 
 # Install Discord
 # https://discord.com/download
 install_discord() {
-    deb="$CACHE_DIR/discord.deb"
-    curl -sSLo "$deb" \
-        https://discord.com/api/download?platform=linux\&format=deb
-    sudo apt install -y "$deb"
-    unset deb
+    if ! is-supported discord; then
+        deb="$CACHE_DIR/discord.deb"
+        curl -sSLo "$deb" \
+            https://discord.com/api/download?platform=linux\&format=deb
+        sudo apt install -y "$deb"
+        unset deb
+    fi
 }
 
 # Install WezTerm
@@ -80,67 +88,76 @@ install_wezterm() {
 # Install Taskwarrior TUI
 # https://github.com/kdheepak/taskwarrior-tui
 install_taskwarrior_tui() {
-    deb="taskwarrior-tui.deb"
-    curl -sSLo "$CACHE_DIR/$deb" "https://github.com/kdheepak/taskwarrior-tui/releases/latest/download/$deb"
-    sudo apt install -y "$CACHE_DIR/$deb" || true
+    if ! is-supported taskwarrior-tui; then
+        deb="taskwarrior-tui.deb"
+        curl -sSLo "$CACHE_DIR/$deb" "https://github.com/kdheepak/taskwarrior-tui/releases/latest/download/$deb"
+        sudo apt install -y "$CACHE_DIR/$deb" || true
+    fi
 }
 
 # Install Docker Desktop
 # https://docs.docker.com/desktop/install/ubuntu/
 install_docker_desktop() {
-    deb="docker-desktop-4.16.2-amd64.deb"
-    # rm -r ~/.docker/desktop
-    # sudo rm /usr/local/bin/com.docker.cli
-    # sudo apt remove docker-desktop
-    # sudo apt purge docker-desktop
-    curl -sSLo "$CACHE_DIR/$deb" \
-        "https://desktop.docker.com/linux/main/amd64/$deb"
-    sudo apt install "$CACHE_DIR/$deb" || true
-    unset deb
+    if ! is-supported docker-desktop; then
+        deb="docker-desktop-4.16.2-amd64.deb"
+        rm -r ~/.docker/desktop || true
+        sudo rm /usr/local/bin/com.docker.cli || true
+        sudo apt purge docker-desktop || true
+        curl -sSLo "$CACHE_DIR/$deb" \
+            "https://desktop.docker.com/linux/main/amd64/$deb"
+        sudo apt install "$CACHE_DIR/$deb" || true
+        unset deb
+    fi
 }
 
 # Install Docker Engine
 # https://docs.docker.com/engine/install/ubuntu/
 install_docker_engine() {
-    sudo mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
-        sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg \
-            sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    if ! is-supported docker; then
+        sudo mkdir -m 0755 -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
+            sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg \
+                sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-    echo "deb [arch=$(dpkg --print-architecture) \
+        echo "deb [arch=$(dpkg --print-architecture) \
       signed-by=/etc/apt/keyrings/docker.gpg] \
         https://download.docker.com/linux/ubuntu \
         $(lsb_release -cs) stable" |
-        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+            sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-    sudo apt update
-    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin || true
+        sudo apt update
+        sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin || true
+    fi
 }
 
 # Install GitHub CLI
 # https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 install_gh_cli() {
-    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg |
-        sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
-        sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
-        echo "deb [arch=$(dpkg --print-architecture) \
+    if ! is-supported gh; then
+        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg |
+            sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+            sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+            echo "deb [arch=$(dpkg --print-architecture) \
           signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] \
           https://cli.github.com/packages stable main" |
-        sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
-        sudo apt update &&
-        sudo apt install -y gh || true
+            sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+            sudo apt update &&
+            sudo apt install -y gh || true
+    fi
 }
 
 # Install Brave Browser
 # https://brave.com/linux/#debian-ubuntu-mint
 install_brave_browser() {
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg \
-        https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] \
+    if ! is-supported brave-browser; then
+        sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg \
+            https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] \
          https://brave-browser-apt-release.s3.brave.com/ stable main" |
-        sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt update
-    sudo apt install -y brave-browser || true
+            sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+        sudo apt update
+        sudo apt install -y brave-browser || true
+    fi
 }
 
 install_gnome_gruvbox_theme() {
