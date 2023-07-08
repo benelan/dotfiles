02924e3a3c1814dc6e026d94d11763fe54ca8042
Ben Elan <no-reply@benelan.dev>
2023-07-07 18:42:27 -0700

    refactor(prompt): remove nerdfont icons and other cleanup

--- .dotfiles/shell/prompt.sh
+++ .dotfiles/shell/prompt.sh
@@ -11,40 +11,35 @@ bind "set vi-ins-mode-string \"I \""
 
 # use starship for the prompt if installed                    {{{
 if is-supported starship; then
-    # Only use Nerd Font symbols if they are available
-    if ! [ "$USE_DEVICONS" = "0" ] && ([ "$USE_DEVICONS" = "1" ] ||
-        [ "$TERM" == "wezterm" ] || tmux showenv | grep -q 'TERM=wezterm'); then
-        export STARSHIP_CONFIG=~/.config/starship/nerdfont.starship.toml
-    else
-        export STARSHIP_CONFIG=~/.config/starship/starship.toml
-    fi
     eval "$(starship init bash)"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }}}
-
 # otherwise create a prompt manually using ps1                {{{
 else
 
-    # If Bash 4.0 is available, trim very long paths in prompt
+    # trim long paths if possible                   {{{
     if ((BASH_VERSINFO[0] >= 4)); then
         PROMPT_DIRTRIM=4
     fi
 
-    # Highlight the user name when logged in as root.
+    # - - - - - - - - - - - - - - - - - - - - - - - }}}
+    # highlight username when logged in as root     {{{
     if [[ "${USER}" == "root" ]]; then
         userStyle="${BOLD}${RED}"
     else
         userStyle="${ORANGE}"
     fi
 
-    # Highlight the hostname when connected via SSH.
+    # - - - - - - - - - - - - - - - - - - - - - - - }}}
+    # highlight the hostname when connected via SSH {{{
     if [[ "${SSH_TTY}" ]]; then
         hostStyle="${BOLD}${RED}"
     else
         hostStyle="${YELLOW}"
     fi
 
-    # get status of git repo in prompt
+    # - - - - - - - - - - - - - - - - - - - - - - - }}}
+    # git prompt options                            {{{
     export GIT_PS1_SHOWDIRTYSTATE="yes"
     export GIT_PS1_SHOWSTASHSTATE="yes"
     export GIT_PS1_SHOWUNTRACKEDFILES="yes"
@@ -55,6 +50,8 @@ else
     export GIT_PS1_SHOWUPSTREAM="verbose"
     export GIT_PS1_DESCRIBE_STYLE="contains"
 
+    # - - - - - - - - - - - - - - - - - - - - - - - }}}
+    # set the pre_prompt                            {{{
     pre_prompt="\[${RESET}\]\n"
     pre_prompt+="\[${BOLD}\]\[${userStyle}\]\u" # username
     pre_prompt+="\[${RESET}\] at "
@@ -63,11 +60,16 @@ else
     pre_prompt+="\[${BOLD}\]\[${BLUE}\]\w" # working directory full path
     pre_prompt+="\[${RESET}\]"             # reset styling
 
+
+    # - - - - - - - - - - - - - - - - - - - - - - - }}}
+    # set the post_prompt                           {{{
     post_prompt="\n"
     post_prompt+="\[${PURPLE}\][\!]" # history line number for easy hist expansion
     # shellcheck disable=2181,2016
-    post_prompt+='$(if [ $? -ne 0 ]; then printf "\[%s\] ✘  " "$red"; else printf "\[%s\] ❯ " "$green"; fi)'
+    post_prompt+='$(if [ $? -ne 0 ]; then printf "\[%s\] ✘  " "$red"; else printf "\[%s\] ❱ " "$green"; fi)'
     post_prompt+="\[${RESET}\]" # reset styling
+
+    # - - - - - - - - - - - - - - - - - - - - - - - }}}
 
     # shellcheck disable=2154
     PROMPT_COMMAND='__git_ps1 "${pre_prompt}" "${post_prompt}"'
