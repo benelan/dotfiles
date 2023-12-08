70521ebd711141fe17543a1f1346f52cec78e9bb
Ben Elan <no-reply@benelan.dev>
2023-12-08 14:04:10 -0800

    feat(nvim): improve cmds/keys/autocmd for cd'ing to lsp/git root dir
    - Add `Gcd` command and `c/` keymap for changing the directory to the git
      worktree's root
    - Change `Rcd` command and `cr` keymap to `Pcd` and `cp` to prevent
      collision with `cr` keymap in flash.nvim ([R]oot --> [P]roject)
    - Try the LSP root_dir first, then the file marker for `Pcd`
    - Tweak the list of file markers and ignored LSPs for `Pcd`

--- .config/nvim/after/plugin/rooter.lua
+++ .config/nvim/after/plugin/rooter.lua
@@ -4,18 +4,12 @@
 --------------------------------------------------------------------------------
 
 local root_names = {
-  ".git",
   "package.json",
-  ".luarc.json",
-  ".stylua.toml",
-  "go.mod",
-  "Cargo.toml",
   "Dockerfile",
   "Makefile",
-  "CONTRIBUTING.md",
 }
 
-local ignored_lsps = { "null-ls" }
+local ignored_lsps = { "null-ls", "copilot", "eslint" }
 
 -- Cache to use for speed up (at cost of possibly outdated results)
 local root_cache = {}
@@ -30,9 +24,10 @@ local function get_lsp_root(buf, ignore)
 
   for _, client in pairs(clients) do
     if
-      client.config.filetypes
-      and vim.tbl_contains(client.config.filetypes, vim.bo[buf].ft)
+      client.config.filetypes ---@diagnostic disable-line: undefined-field
+      and vim.tbl_contains(client.config.filetypes, vim.bo[buf].ft) ---@diagnostic disable-line: undefined-field
       and not vim.tbl_contains(ignore, client.name)
+      and vim.fn.isdirectory(client.config.root_dir)
     then
       return client.config.root_dir, client.name
     end
@@ -54,8 +49,8 @@ local function set_root(args)
       stop = vim.loop.os_homedir(),
     })[1]
 
-    -- swapping the order will prefer lsp roots vs matches to root_names
-    root = vim.fs.dirname(root_file) or get_lsp_root(args.buf, ignored_lsps)
+    -- swapping the order will prefer matches to root_names vs lsp roots
+    root = get_lsp_root(args.buf, ignored_lsps) or vim.fs.dirname(root_file)
   end
 
   if not root or root == vim.fn.getcwd() then return end
@@ -69,9 +64,29 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "LspAttach" }, {
   callback = set_root,
 })
 
-vim.api.nvim_create_user_command("Rcd", function()
-  set_root { buf = 0, file = vim.fn.expand "%:p" }
+-- Change directory to project root using LSP or file markers in `root_names`
+vim.api.nvim_create_user_command("Pcd", function()
+  set_root { buf = 0, file = vim.api.nvim_buf_get_name(0) }
   vim.cmd "pwd"
 end, { desc = "Change directory to project root" })
 
-vim.keymap.set("n", "cr", "<CMD>Rcd<CR>", { desc = "Change directory to project root" })
+vim.keymap.set("n", "cp", "<CMD>Pcd<CR>", { desc = "Change directory to project (lsp) root" })
+
+-- Change directory to the git worktree's root.
+-- This is useful when working with monorepos, where the project root is not always the git root.
+vim.api.nvim_create_user_command("Gcd", function()
+  local worktree_root = vim.g.loaded_fugitive and vim.fn.FugitiveFind ":/"
+    or vim.fn.trim(
+      vim.fn.system(
+        "git -C "
+          .. vim.fn.shellescape(vim.fs.dirname(vim.api.nvim_buf_get_name(0))) ---@diagnostic disable-line: param-type-mismatch
+          .. " rev-parse --show-toplevel"
+      )
+    )
+
+  if vim.v.shell_error == 0 and vim.fn.isdirectory(worktree_root) == 1 then
+    vim.cmd("chdir " .. worktree_root)
+  end
+end, { desc = "Change directory to the git repo root" })
+
+vim.keymap.set("n", "c/", "<CMD>Gcd<CR>", { desc = "Change directory to git worktree root" })
