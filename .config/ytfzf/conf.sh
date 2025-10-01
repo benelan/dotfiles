c3943ca7a174621f2891b5568afae8c42d6340e3
Ben Elan <no-reply@benelan.dev>
2025-10-01 06:57:40 -0700

    fix(ytfzf): self-host invidious, don't focus thumbnail

--- .config/ytfzf/conf.sh
+++ .config/ytfzf/conf.sh
@@ -4,9 +4,13 @@
 
 is_sort=1
 is_loop=1
+async_thumbnails=1
+thumbnail_viewer="mpv"
 fzf_preview_side="right"
 next_page_action_shortcut="alt-p"
-thumbnail_viewer="mpv"
-async_thumbnails=1
+
+if curl -sfw "%{exitcode}" "http://media:3333" >/dev/null 2>&1; then
+    invidious_instance=http://media:3333
+fi
 
 external_menu() { rofi -dmenu -width 1500 -p "$1"; }
